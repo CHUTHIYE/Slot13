19acec5bffc3a39921846bc56086aec99d23a5b1
Chu Thị Yên <119092877+CHUTHIYE@users.noreply.github.com>
2023-03-17 14:24:40 +0700

    slot13-Student

---
+++ com.example2/entity/Student.java
@@ -0,0 +1,55 @@
+package com.example2.entity;
+
+import java.util.Scanner;
+
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private double mark;
+
+    public Student() {
+
+    }
+
+    public Student(int id, String firstName, String lastName, double mark) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mark = mark;
+    }
+
+    public void scanInfo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter ID: ");
+        this.id = sc.nextInt();
+        System.out.println("Enter first name: ");
+        this.firstName = sc.nextLine();
+        System.out.println("Enter last name: ");
+        this.lastName = sc.nextLine();
+        System.out.println("Enter mark: ");
+        this.mark = sc.nextDouble();
+
+    }
+
+    public void printInfo() {
+        System.out.println();
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+}
